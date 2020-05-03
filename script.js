41c36ef1fdc14442ff527974f21a43550bc4434e
darosg1 <darosg1@yahoo.com>
2020-05-03 18:51:01 +0200

    9th commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://cors-anywhere.herokuapp.com/https://immense-hamlet-14304.herokuapp.com/v1/task/';
+  var apiRoot = 'https://immense-hamlet-14304.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
